773b2646341e12cbd4af0ead0821ac69875dbfae
minister2020 <abdulrasheedidowu86@gmail.com>
2025-11-16 16:10:07 +0000

    change prod url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'javabackendpaymentregistrationportal-production.up.railway.app/api';
+const API_BASE_URL = 'https://payment-registration-portal.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
